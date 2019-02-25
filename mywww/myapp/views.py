362bd6a3e91cde20b16018fa93f449a8df34404d
matrixloong <matrixloong@126.com>
2019-02-25 11:40:02 +0800

    First Time Committing

---
+++ mywww/myapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.template.loader import get_template
+from django.http import HttpResponse
+from datetime import datetime
+# Create your views here.
+
+
+def index(request):
+    template = get_template('index.html')
+    now = datetime.now()
+    html = template.render(locals())
+
+    return HttpResponse(html)
